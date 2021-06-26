f1ed6190436944d38823ff6a725358f440f21cca
Vinicius Silva <vinicius.silva.89@outlook.com>
2021-06-26 11:02:26 -0300

    Aula05_NLW_completa
    E esse foi o final do projeto da minha primeira semana da NLW da Rocketseat. Somando muito aprendizado de Front-End.
    
    O resultado ficou um pouco diferente do original, em alguns espaçamentos e principalmente no efeito do 'menu', onde dependendo da sessão que o usuário estiver, fica visível a mesma linha que surge ao clicarmos em um item dele.
    
    Mesmo desenvolvedores com muito tempo de estrada se confundem no código, como já vi em várias aulas, mas uma skill se destaca na ocasião e que está ligada a um bom programador: a capacidade de identificar e resolver esses problemas.
    
    Fica aí a anotação e o motivo de voltar e tentar arrumar melhor o código.

--- NLW/main.js
+++ NLW/main.js
@@ -50,7 +50,13 @@ const swiper = new Swiper('.swiper-container', {
         el: '.swiper-pagination'
     },
     mousewheel: true,
-    keyboard: true
+    keyboard: true,
+    breakpoints: {
+        767: {
+            slidesPerView: 2,
+            setWrapperSize: true
+        }
+    }
 })
 
 /* ScrollReveal: Mostrar elementos quando der scroll na página */
@@ -81,3 +87,31 @@ window.addEventListener('scroll', function(){
         backToTopButton.classList.remove('show')
     }
 })
+
+/* Menu ativo  conforme  a seção  visível na página */
+const section = document.querySelectorAll('main section[id]')
+function activateMenuAtCurrentSection(){
+
+    const checkpoint = window.pageYOffset + (window.innerHeight / 8) * 4
+
+    for( const section of sections ){
+        const sectionTop = section.offsetTop
+        const sectionHeight = section.offsetHeight
+        const sectionId = section.getAttribute('id')
+
+        const checkpointStart = checkpoint >= sectionTop
+        const checkpointEnd = checkpoint <= sectionTop + sectionHeight
+
+        if(checkpointStart && checkpointEnd){
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.add('active')
+
+        } else{
+            document
+            .querySelector('nav ul li a[href*=' + sectionId + ']')
+            .classList.remove('active')
+        }
+    }
+}
+
